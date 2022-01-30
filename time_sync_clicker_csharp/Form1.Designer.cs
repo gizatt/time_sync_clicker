c29bcb7430399302d07004718e68d9982367afcb
gizatt <gregory.izatt@mit.edu>
2022-01-29 20:01:01 -0500

    Add project files.

---
+++ time_sync_clicker_csharp/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace time_sync_clicker_csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_schedule_click = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label_time_to_click = new System.Windows.Forms.Label();
+            this.label_time = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_schedule_click
+            // 
+            this.btn_schedule_click.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_schedule_click.Location = new System.Drawing.Point(6, 13);
+            this.btn_schedule_click.Name = "btn_schedule_click";
+            this.btn_schedule_click.Size = new System.Drawing.Size(177, 43);
+            this.btn_schedule_click.TabIndex = 0;
+            this.btn_schedule_click.Text = "Queue Synchro-click";
+            this.btn_schedule_click.UseVisualStyleBackColor = true;
+            this.btn_schedule_click.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Info;
+            this.label8.Location = new System.Drawing.Point(20, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Clicking in:";
+            // 
+            // label_time_to_click
+            // 
+            this.label_time_to_click.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label_time_to_click.Location = new System.Drawing.Point(84, 76);
+            this.label_time_to_click.Name = "label_time_to_click";
+            this.label_time_to_click.Size = new System.Drawing.Size(83, 13);
+            this.label_time_to_click.TabIndex = 9;
+            this.label_time_to_click.Text = "<>";
+            // 
+            // label_time
+            // 
+            this.label_time.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label_time.Location = new System.Drawing.Point(51, 61);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(83, 13);
+            this.label_time.TabIndex = 10;
+            this.label_time.Text = "<>";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 111);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.label_time_to_click);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_schedule_click);
+            this.Name = "Form1";
+            this.Text = "Synchroclicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_schedule_click;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label_time_to_click;
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ time_sync_clicker_csharp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace time_sync_clicker_csharp
+{
+    public partial class Form1 : Form
+    {
+        private System.DateTime click_time;
+        public Form1()
+        {
+            InitializeComponent();
+            //this.BackColor = Color.LimeGreen;
+            //this.TransparencyKey = Color.LimeGreen;
+            
+            this.click_time = DateTime.MinValue;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Figure out when to click; want to click at an
+            // even 10 second interval more than 3 seconds in
+            // the future.
+            var now = DateTime.Now;
+
+            int next_10s_secs = 10 - (now.Second % 10);
+            var new_click_time = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            if (next_10s_secs <= 3) {
+                next_10s_secs += 10;
+            }
+            this.click_time = new_click_time.AddSeconds(next_10s_secs);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            var now = DateTime.Now;
+            label_time.Text = now.ToString("hh:mm:ss tt");
+
+            if (click_time != DateTime.MinValue)
+            {
+                if (click_time > now)
+                {
+                    label_time_to_click.Text = (this.click_time - now).TotalSeconds.ToString("0.##");
+                }
+                else
+                {
+                    uint X = (uint) Cursor.Position.X;
+                    uint Y = (uint) Cursor.Position.Y;
+                    Win32.mouse_event(Win32.MOUSEEVENTF_LEFTDOWN | Win32.MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+
+                    this.click_time = DateTime.MinValue;
+                    label_time_to_click.Text = "Clicked!";
+                }
+            }
+        }
+        public class Win32
+        {
+            [DllImport("User32.Dll")]
+            public static extern long SetCursorPos(int x, int y);
+
+            [DllImport("User32.Dll")]
+            public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+            public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+            //Mouse actions
+            public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+            public const int MOUSEEVENTF_LEFTUP = 0x04;
+            public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+            public const int MOUSEEVENTF_RIGHTUP = 0x10;
+            // Absolute coords
+            public const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct POINT
+            {
+                public int x;
+                public int y;
+            }
+        }
+    }
+
+}
